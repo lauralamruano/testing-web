d9aac18df1356a93fd34a6414760e3ddcf8a64bb
lauralamruano <lamruanolaura@gmail.com>
2024-08-21 10:02:58 -0400

    fix: Format to HomePage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,7 @@
 import Link from "next/link";
 import React from "react";
 
-
-
 const HomePage = () => {
-
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-centerp-24">
