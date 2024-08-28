efa81704742e03dd11b96dcf4cbd9f57c0c2dd02
lauralamruano <lamruanolaura@gmail.com>
2024-08-28 17:26:25 -0400

    feature: Add Timing Alert Component

--- app/(home)/timing-adjustable/page.tsx
+++ app/(home)/timing-adjustable/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import TimeLimitForm from "@/components/Timing/timing-adjustable";
+import TimeAlertForm from "@/components/Timing/timing-alert";
 import React from "react";
 
 const CheckPoints = () => {
 
     return (
-      <main className="min-h-screen flex items-center justify-center p-24">
+      <main className="min-h-screen flex items-center justify-center flex-wrap gap-6 p-24">
         <TimeLimitForm/>
+        <TimeAlertForm/>
       </main>
     );
   };
