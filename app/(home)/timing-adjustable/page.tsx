88b34c195454ea41840abf65d506adde418e8f10
lauralamruano <lamruanolaura@gmail.com>
2024-08-28 17:34:10 -0400

    fix: style and format in Timing Page

--- app/(home)/timing-adjustable/page.tsx
+++ app/(home)/timing-adjustable/page.tsx
@@ -4,14 +4,18 @@ import TimeAlertForm from "@/components/Timing/timing-alert";
 import React from "react";
 
 const CheckPoints = () => {
-
-    return (
-      <main className="min-h-screen flex items-center justify-center flex-wrap gap-6 p-24">
-        <TimeLimitForm/>
-        <TimeAlertForm/>
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center"> 
+      <header>
+        {" "}
+        <h1 className="text-4xl font-bold mb-6 p-6">Timing Adjustable</h1>
+      </header>
+      <main className=" flex flex-wrap gap-6 p-24">
+        <TimeLimitForm />
+        <TimeAlertForm />
       </main>
-    );
-  };
-  
-  export default CheckPoints;
-  
+    </div>
+  );
+};
+
+export default CheckPoints;
