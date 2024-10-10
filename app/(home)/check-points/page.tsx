f1219277b7c3ec02f8a91333b73507c86d87cd31
lauralamruano <lamruanolaura@gmail.com>
2024-10-10 16:10:04 -0400

    fix: formating in Check Pointes page

--- app/(home)/check-points/page.tsx
+++ app/(home)/check-points/page.tsx
@@ -6,6 +6,19 @@ const CheckPoints = () => {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-centerp-24">
+      <h1 id="accessibility-table-description"
+          className="text-4xl font-bold mb-4 text-green-dark mt-8">Accessibility Checkpoints</h1>
+      <section className="flex items-center bg-green-dark-50 p-8">
+        <p className="text-lg ml-8 text-green-dark font-bold ">
+          The section titled "Accessibility Checkpoints" is designed to provide
+          users with a clear and concise understanding of accessibility success
+          criteria according to the WCAG guidelines. This section includes a
+          table that lists each checkpoint, along with practical examples
+          demonstrating how these criteria can be implemented in web
+          applications.
+        </p>
+        <img className="w-6/12" src="/images/women-mobile.png" alt="" />
+      </section>
       <CheckListTable/>
     </main>
   );
--- components/table/table-check-list.tsx
+++ components/table/table-check-list.tsx
@@ -3,22 +3,9 @@ import React from "react";
 
 const CheckListTable = () => {
   return (
-    <div className="table-container flex flex-col items-center overflow-x-auto mt-8">
-      <h1 id="accessibility-table-description"
-          className="text-lg font-bold mb-4 text-green-dark">Accessibility Checkpoints</h1>
-      <section className="flex items-center bg-green-dark-50 p-8">
-        <p className="text-lg ml-8 text-green-200 font-bold">
-          The section titled "Accessibility Checkpoints" is designed to provide
-          users with a clear and concise understanding of accessibility success
-          criteria according to the WCAG guidelines. This section includes a
-          table that lists each checkpoint, along with practical examples
-          demonstrating how these criteria can be implemented in web
-          applications.
-        </p>
-        <img className="w-6/12" src="/images/women-mobile.png" alt="" />
-      </section>
+    <div className="table-container flex flex-col items-center overflow-x-auto mt-8 w-full">
       <table
-        className="min-w-full border-collapse border border-gray-200  bg-green-dark-50"
+        className="min-w-full border-collapse border border-gray-200  bg-green-dark"
         aria-describedby="accessibility-table-description"
       >
         <thead>
