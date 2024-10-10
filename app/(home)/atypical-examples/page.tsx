26170a17e6d670a9d26776bd0e19d788e7b56cee
lauralamruano <lamruanolaura@gmail.com>
2024-10-10 14:42:01 -0400

    fix: styles for atypical examples page

--- app/(home)/atypical-examples/page.tsx
+++ app/(home)/atypical-examples/page.tsx
@@ -4,9 +4,11 @@ import PatternCard from "@/components/card/card-pattern";
 const AtypicalExamples: React.FC = () => {
   return (
     <main className="flex min-h-screen items-center justify-center flex-col">
-      <h1 className="text-lg font-bold mt-4 text-green-dark">Accessible Atypical Examples</h1>
+      <h1 className="text-xl font-bold mt-4 text-green-dark">
+        Accessible Atypical Examples
+      </h1>
       <section className="flex items-center bg-green-dark-50 p-12">
-      <img className="w-6/12" src="/images/women-mobile.png" alt="" />
+        <img className="w-6/12" src="/images/women-mobile.png" alt="" />
         <p className="text-lg mr-8 text-green-dark font-bold">
           The section titled "Accessible Atypical Examples" is designed to
           showcase unique and creative implementations of accessibility that go
@@ -18,26 +20,33 @@ const AtypicalExamples: React.FC = () => {
           unexpected and effective ways.
         </p>
       </section>
-      <section className="flex flex-col items-center justify-center pb-4"> 
-        <h2 className="text-lg font-bold mt-4 text-green-200 mb-6">Accessibility Examples</h2>
-      <ul className="flex gap-8">
-        <li>
-          <PatternCard
-            title="Order Table"
-            description="This example will show a table with sorting and resizing of columns using Keyboard."
-            imageSrc="/images/order-table.png"
-            link="./ordertable"
-          />
-        </li>
-        <li>
-          <PatternCard
-            title="Upload Image"
-            description="This example is a  file export component allows users to export files using keyboard navigation."
-            imageSrc="/images/update-images.png"
-            link=""
-          />
-        </li>
-      </ul>
+      <section className="flex flex-col items-center justify-center pb-8 bg-green-dark w-full">
+        <h2 className="text-lg font-bold mt-4 text-green-200 mb-6">
+          Accessibility Examples
+        </h2>
+        <ul className="flex gap-8">
+          <li>
+            <PatternCard
+              title="Order Table"
+              description="This example will show a table with sorting and resizing of columns using Keyboard."
+              imageSrc="/images/order-table.png"
+              link="./ordertable"
+            />
+          </li>
+          <li>
+            <PatternCard
+              title="Upload Image"
+              description="This example is a  file export component allows users to export files using keyboard navigation."
+              imageSrc="/images/update-images.png"
+              link=""
+            />
+          </li>
+        </ul>
+      </section>
+      <section className="flex flex-col items-center justify-center pb-4 w-full">
+      <h2 className="text-lg font-bold mt-4 text-green-200 mb-6">
+          Accessibility Issues
+        </h2>
       </section>
     </main>
   );
