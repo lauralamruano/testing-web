27d9325a672ef1b059e419c827733a0002fbc960
lauralamruano <lamruanolaura@gmail.com>
2025-03-10 16:49:42 -0400

    fix: styles tabindex

--- app/(home)/focus-order/tabindex/page.tsx
+++ app/(home)/focus-order/tabindex/page.tsx
@@ -6,7 +6,7 @@ const Tabindex: React.FC = () => {
   return (
     <div>
       <h1  className="text-4xl font-bold mt-4 text-green-dark">Prueba de Accesibilidad - Tabindex</h1>
-      <section id="tabindex-1">
+      <section className="flex flex-wrap items-center justify-center gap-4 bg-green-dark w-full p-4" id="tabindex-1">
       <PatternCard title={"Tabindex Negative"}
             description={"These examples  have controls are Nested Focus"}
             link={"/focus-order/tabindex/tabindex-negative"}
